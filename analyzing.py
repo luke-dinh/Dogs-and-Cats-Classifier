0a014bf879640398791c5141de58b20f37f165ed
luke-dinh <dinhphuoclockt@gmail.com>
2020-07-01 13:52:55 +0700

    add new files

---
+++ analyzing.py
@@ -0,0 +1,47 @@
+from keras.datasets import cifar10
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import TensorBoard
+import pickle
+import time
+
+NAME = 'Cat_vs_Dog_CNN'
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+model = Sequential()
+
+model.add(Convolution2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(256, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+tensorboard = TensorBoard(log_dir="logs\{}".format(NAME))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'],
+              )
+
+model.fit(X, y,
+          batch_size=32,
+          epochs=3,
+          validation_split=0.3,
+          callbacks=[tensorboard])
